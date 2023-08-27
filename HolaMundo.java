277fa267c7e6e21a396ea6cbac328e6efd923301
José <jose.lara.a01@mail.pucv.cl>
2023-08-27 04:27:19 -0400

    Co-authored-by: Nikolai Navea <NikoNavea@users.noreply.github.com>

---
+++ HolaMundo.java
@@ -0,0 +1,10 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, mundo!");
+        int a = 5;
+        System.out.println(a);
+        int b = 10;
+        System.out.println(b);
+        System.out.println("¡Hola, mundo!");
+    }
+}
